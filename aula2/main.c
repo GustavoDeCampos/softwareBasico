a7025bca3592df9c50901486336fc001bc785f50
GustavoDeCampos <161768375+GustavoDeCampos@users.noreply.github.com>
2024-08-29 23:22:40 +0000

    exercicio

---
+++ aula2/main.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+
+int funcVet(int vetor[], int tamanho){
+    int i, j = 0;
+    int repetidos = 0;
+
+
+    for (i = 0; i < tamanho; i++){
+        int atual = vetor[i];
+        for (j = i + 1; j < tamanho; j++){
+            int possivelRepetido = vetor[j];
+            if (atual == possivelRepetido){
+                repetidos++;
+            }
+        }
+    }
+    return repetidos;
+}    
+
+int main(){
+    int vetor[] = {4, 2, 3, 4, 5, 3, 5, 7, 2, 8};
+    int tamanho = sizeof(vetor)/sizeof(vetor[0]);
+
+    int resultado = funcVet(vetor, tamanho);
+    printf("Resultado: %d\n", resultado);
+
+    return 0;
+}
